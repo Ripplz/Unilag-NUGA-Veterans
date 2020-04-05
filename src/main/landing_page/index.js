8f3baa5f471570298534a1e6748e115144d9cc4b
Stephen Ojo <ojostephendev@gmail.com>
2020-04-05 16:04:05 +0100

    Added homepage instructions

--- src/main/landing_page/index.js
+++ src/main/landing_page/index.js
@@ -7,15 +7,18 @@ const LandingPage = () => {
     <div id="wrapper_landing_page">
       <div id="img_landing_page_header" />
       <div id="landing_page_header">
-        <div id="landing_page_header_title">Instructions</div>
+        <div id="landing_page_header_title">
+          Welcome to the home page of UNILAG NUGA BASKETBALL VETERANS
+        </div>
         <div id="landing_page_header_instructions">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Over the last 40+ years unilag has been home to great basketball
+          players, a good number featuring in national teams, state teams and
+          reputable clubs. History will not be kind to us if we do not document
+          the exploits of those who did the university proud. That's what this
+          site is all about. Please fill in your details if you represented
+          unilag in any NUGA, West African University, or African University
+          Games. The veracity of all entries will be checked with other players.
+          Please post current pictures and throwback pictures.
         </div>
       </div>
     </div>
